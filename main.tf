b5691e4d1268830d9156f504e08899564e52b693
Na Li <arcanens@gmail.com>
2022-12-13 14:08:20 +0100

    commented VPC block

--- main.tf
+++ main.tf
@@ -57,22 +57,22 @@ resource "aws_instance" "web" {
               EOF
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+// resource "aws_security_group" "web-sg" {
+//  name = "${random_pet.sg.id}-sg"
+//  ingress {
+//    from_port   = 8080
+//    to_port     = 8080
+//    protocol    = "tcp"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+//  egress {
+//    from_port   = 0
+//    to_port     = 0
+//    protocol    = "-1"
+//    cidr_blocks = ["0.0.0.0/0"]
+//  }
+//}
 
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
