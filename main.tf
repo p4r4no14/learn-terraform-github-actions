f17399d279a412387a806dc748c7f041481a4a1b
Na Li <arcanens@gmail.com>
2022-12-13 14:21:20 +0100

    adjustments

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
       version = "3.0.1"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = "~> 1.0"
 
   cloud {
     organization = "paracode"
@@ -20,9 +20,12 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region = "us-east-1"
 }
+
+
 
 resource "random_pet" "sg" {}
 
@@ -33,27 +36,17 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
               EOF
 }
 
- resource "aws_security_group" "web-sg" {
+resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
