2b96ff0e695e17f80c878da7ed1ffefb7622f7ff
p4r4no14 <102704130+p4r4no14@users.noreply.github.com>
2022-12-13 13:48:19 +0100

    Revert "VPC setup"

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "web-sg" {
-  name = "VPC_test_us"
+  name = "${random_pet.sg.id}-sg"
   ingress {
     from_port   = 8080
     to_port     = 8080
