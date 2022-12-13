31822829b4a599efe8cad201299dbad409b2e039
Na Li <arcanens@gmail.com>
2022-12-13 14:15:52 +0100

    config change

--- main.tf
+++ main.tf
@@ -26,24 +26,8 @@ provider "aws" {
 
 resource "random_pet" "sg" {}
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
-  ami                    = "data.aws_ami.ubuntu.id"
+  ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
@@ -57,22 +41,22 @@ resource "aws_instance" "web" {
               EOF
 }
 
-// resource "aws_security_group" "web-sg" {
-//  name = "${random_pet.sg.id}-sg"
-//  ingress {
-//    from_port   = 8080
-//    to_port     = 8080
-//    protocol    = "tcp"
-//    cidr_blocks = ["0.0.0.0/0"]
-//  }
+ resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-//  egress {
-//    from_port   = 0
-//    to_port     = 0
-//    protocol    = "-1"
-//    cidr_blocks = ["0.0.0.0/0"]
-//  }
-//}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
